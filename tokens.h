969a69fcc016ea3d68ca4a4393fc82ad5b7e8945
Justin Smith <justin.t.smith87@gmail.com>
2025-03-23 13:36:23 -0400

    Add files via upload
    source code for lex project

---
+++ tokens.h
@@ -0,0 +1,11 @@
+// CMSC 430 Compiler Theory and Design
+// Project 1 Skeleton
+// UMGC CITE
+// Summer 2023
+
+// This file contains the enumerated type definition for tokens used by the lex file
+
+enum Tokens {ADDOP = 256, MULOP, ANDOP, RELOP, ARROW, BEGIN_, CASE, CHARACTER, END,
+     ENDSWITCH, FUNCTION, INTEGER, IS, LIST, OF, OTHERS, RETURNS, SWITCH, WHEN, ELSE,
+     ELSIF, ENDFOLD, ENDIF, FOLD, IF, LEFT, RIGHT, THEN, REAL, IDENTIFIER, INT_LITERAL, 
+     CHAR_LITERAL, OROP, NOTOP, REMOP, EXPOP, NEGOP, REAL_LITERAL, HEX_LITERAL};
